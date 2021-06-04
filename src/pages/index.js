e2194eea90418558aef46e257a9174152eaec0fb
Andrii Vasylchenko <andrii.vasylchenko@euclidsys.com>
2021-06-04 16:50:24 -0400

    title change

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ export default function IndexPage() {
   return (
     <ThemeProvider theme={theme}>
         <Layout>
-          <SEO title="NextJS Landing Page" />
+          <SEO title="Meme Shop" />
           <Banner />
           <KeyFeature />
           <ServiceSection />
